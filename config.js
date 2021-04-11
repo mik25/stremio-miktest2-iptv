538a0fa1dd71e44ab45c59eec8a7ff17fd8b87b2
mik25 <mikmc55@gmail.com>
2021-04-11 17:57:52 +0100

    change

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'MIKTEST IPTV', // unique addon name
+  prefix: 'MIKTEST IPTV', // unique addon prefix (must be different for all addons!)
+  icon: 'https://MIKTEST.surge.sh/logo.png', // set the correct domain name here so the logo is accessible
+  description: 'MIKTEST IPTV', // addon description
+  connLimit: false, // can be false or an integer, if set to an integer, it will show a message with "Limited to X connections."
+  paginate: 100, // how many catalogs to show per page
+  playlistUrl: 'http://m3u4u.com/m3u/69wknggr4za6q9qnq8gd', // the URL to the M3U playlist
+}
